a0e2f0689862cdac6981afe88893d0bc47ddb122
Eoin Hurrell <ultimatehurl@gmail.com>
2013-12-29 19:55:42 +0000

    Modularised sentiment, added wordlist license.

---
+++ __init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""Module to provide basic sentiment analysis for given text.
+
+Author: Eoin Hurrell <UltimateHurl@gmail.com>
+Created: 13-12-27
+
+"""
+from bagOfWords import *
---
+++ bagOfWords.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""Module to provide basic sentiment analysis for given text.
+
+Author: Eoin Hurrell <UltimateHurl@gmail.com>
+Created: 13-12-27
+
+"""
+import sys
+import os
+
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+POSFILE = os.path.join(__location__, 'wordlists/positive-words.txt')
+POSWORDS = []
+NEGFILE = os.path.join(__location__, 'wordlists/negative-words.txt')
+NEGWORDS = []
+
+
+def loadSentiment():
+    global POSWORDS
+    global NEGWORDS
+    for line in open(POSFILE, 'r'):
+        word = line.strip().decode('utf-8')
+        POSWORDS.append(word)
+    for line in open(NEGFILE, 'r'):
+        word = line.strip().decode('utf-8')
+        NEGWORDS.append(word)
+
+def calculateBOWSentiment(text):
+    global HITS
+    text = text.encode('utf-8')
+    pos = 0
+    neg = 0
+    for x in POSWORDS:
+        hitCount = text.count(x + ' ')
+        if hitCount != 0: print x
+        pos += hitCount
+    for x in NEGWORDS:
+        hitCount = text.count(x + ' ')
+        if hitCount != 0: print x
+        neg += hitCount
+    return pos - neg
+
+
+def getPositiveBOWTerms(text):
+    hits = []
+    for x in POSWORDS:
+        if (x  + ' ') in text and x not in hits:
+            hits.append(x)
+    return hits
+
+
+def getNegativeBOWTerms(text):
+    hits = []
+    for x in NEGWORDS:
+        if (x  + ' ') in text and x not in hits:
+            hits.append(x)
+    return hits
+
+
+loadSentiment()
